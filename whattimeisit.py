be29e8ecd365a9854faaf482c87c89176d0b2e34
Michal Migurski <mike@stamen.com>
2010-09-17 17:12:47 -0700

    No args is okay too

--- whattimeisit.py
+++ whattimeisit.py
@@ -1,15 +1,22 @@
-from dwim import time2str, str2time
+from time import time
 from sys import stderr, argv
+from dwim import time2str, str2time
 from dateutil.tz import tzutc, tzlocal
 
-for arg in argv[1:]:
-    if not arg.isdigit():
-        t = str2time(arg)
-        arg = t
-        
-        print >> stderr, 'time: ', t
+if len(argv) == 1:
+    arg = str(int(time()))
+    print >> stderr, 'time: ', arg
 
-    print >> stderr, 'local:', time2str(int(arg), False, tzlocal())
-    print >> stderr, '      ', time2str(int(arg), True, tzlocal())
-    print >> stderr, 'utc:  ', time2str(int(arg), False, tzutc())
-    print >> stderr, '      ', time2str(int(arg), True, tzutc())
+else:
+    arg = argv[1]
+
+if not arg.isdigit():
+    t = str2time(arg)
+    arg = t
+    
+    print >> stderr, 'time: ', t
+
+print >> stderr, 'local:', time2str(int(arg), False, tzlocal())
+print >> stderr, '      ', time2str(int(arg), True, tzlocal())
+print >> stderr, 'utc:  ', time2str(int(arg), False, tzutc())
+print >> stderr, '      ', time2str(int(arg), True, tzutc())
